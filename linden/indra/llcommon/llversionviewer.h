7f5d0a2c442c73b3cbf25f7a038efb822b0ccff4
McCabe Maxsted <hakushakukun@gmail.com>
2009-09-01 15:40:42 -0700

    Fixed Imprudence version

--- linden/indra/llcommon/llversionviewer.h
+++ linden/indra/llcommon/llversionviewer.h
@@ -42,7 +42,7 @@ const S32 LL_VERSION_BUILD = 0;
 
 const char * const IMP_VIEWER_NAME = "Imprudence";
 const S32 IMP_VERSION_MAJOR = 1;
-const S32 IMP_VERSION_MINOR = 1;
+const S32 IMP_VERSION_MINOR = 2;
 const S32 IMP_VERSION_PATCH = 0;
 const char * const IMP_VERSION_TEST = "";
 
